08ba908e3181df8bab08b2cfc72ba5f39dddcff7
Manas Paranjape <paranjmm@rose-hulman.edu>
2026-02-01 15:51:31 -0500

    Progress commit

--- src/app/Zombie.java
+++ src/app/Zombie.java
@@ -1,10 +1,29 @@
 package app;
 
 public class Zombie extends Mobile {
+	Direction traverseDirection;
+	int traverseLength;
+	int startingX;
+	int startingY;
+	
+	public Zombie(int startingX, int startingY, Direction traverseDirection, int traverseLength); {
+		this.posX = startingX;
+		this.posY = startingY;
+		this.startingX = startingX;
+		this.startingY = startingY;
+		this.traverseDirection = traverseDirection;
+		this.traverseLength = traverseLength;
+	}
+	
 	@Override
 	void update() {
-		// TODO Auto-generated method stub
-		
+		if (traverseDirection == Direction.DOWN) {
+			if (startingY < posY < traverseLength + startingY) {
+				posY += delta;
+			} else {
+				tra
+			}
+		}
 	}
 
 }
