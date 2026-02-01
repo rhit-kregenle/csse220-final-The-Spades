3cb2cb7738e18e153baeb1ce9fc6ced708055989
rhit-kregenle <kregenle@rose-hulman.edu>
2026-02-01 16:34:51 -0500

    half made player ui

--- src/app/Player.java
+++ src/app/Player.java
@@ -7,12 +7,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Player extends Mobile{
+public class Player extends Mobile {
 	private int maxLives;
 	private int currentLives;
 	private static BufferedImage sprite = null;
 	private static boolean triedLoad = false;
-	
+	Direction direction;
+
 	public Player(int startingX, int startingY, int maxLives, int sizeX, int sizeY) {
 		this.posX = startingX;
 		this.posY = startingY;
@@ -20,50 +21,48 @@ public class Player extends Mobile{
 		this.currentLives = maxLives;
 		this.sizeX = sizeX;
 		this.sizeY = sizeY;
+		this.direction = Direction.UP;
 		loadSpriteOnce();
 	}
-	
+
 	private static void loadSpriteOnce() {
-		if (triedLoad) return;
+		if (triedLoad)
+			return;
 		triedLoad = true;
 
 		try {
-		// tennis.png must be in the SAME package as Ball.java
-		sprite = ImageIO.read(Player.class.getResource("tennis.png"));
+			// tennis.png must be in the SAME package as Ball.java
+			sprite = ImageIO.read(Player.class.getResource("tennis.png"));
 		} catch (IOException | IllegalArgumentException ex) {
-		sprite = null; 
+			sprite = null;
 		}
 	}
-	
+
 	public void handleInput(Direction dir) {
-		if (dir == Direction.UP) {
-			posY += delta;
-		}
-		else if (dir == Direction.DOWN) {
-			posY -= delta;
-		}
-		else if (dir == Direction.LEFT) {
-			posX -= delta;
-		}
-		else if (dir == Direction.RIGHT) {
-			posX += delta;
-		}
-		
+		direction = dir;
 	}
-	
+
 	public void draw(Graphics2D g2) {
 
 		if (sprite != null) {
-		// sprite replaces the circle
-		g2.drawImage(sprite, posX, posY, sizeX, sizeY, null);
+			// sprite replaces the circle
+			g2.drawImage(sprite, posX, posY, sizeX, sizeY, null);
 		} else {
-		// fallback if sprite failed to load
-		g2.setColor(Color.RED);
-		g2.fillOval(posX, posY, sizeX, sizeY);
+			// fallback if sprite failed to load
+			g2.setColor(Color.RED);
+			g2.fillOval(posX, posY, sizeX, sizeY);
 		}
-		}
-	
+	}
+
 	public void update() {
-		
+		if (direction == Direction.UP) {
+			posY += delta;
+		} else if (direction == Direction.DOWN) {
+			posY -= delta;
+		} else if (direction == Direction.LEFT) {
+			posX -= delta;
+		} else if (direction == Direction.RIGHT) {
+			posX += delta;
+		}
 	}
 }
--- src/ui/GameComponent.java
+++ src/ui/GameComponent.java
@@ -1,22 +1,56 @@
 package ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JComponent;
+import javax.swing.Timer;
 
+import app.Direction;
+import app.Player;
 import model.GameModel;
 
 public class GameComponent extends JComponent {
 
 	
-	
+	private Player player = new Player(300,500,4,20,20);
+	private Timer timer;
 	private GameModel model;
 
 
 	public GameComponent(GameModel model) {
+
+		setFocusable(true);
+		
+		timer = new Timer(50, e -> {
+			player.update();
+			repaint();
+			});
+		timer.start();
+		
+		addKeyListener(new KeyAdapter() {
+			  @Override
+			  public void keyPressed(KeyEvent e) {
+			    if (e.getKeyCode() == KeyEvent.VK_W) {
+			      player.handleInput(Direction.UP);
+			    }
+			    else if (e.getKeyCode() == KeyEvent.VK_S) {
+				      player.handleInput(Direction.DOWN);
+				}
+			    else if (e.getKeyCode() == KeyEvent.VK_A) {
+				      player.handleInput(Direction.LEFT);
+				}
+			    else if (e.getKeyCode() == KeyEvent.VK_D) {
+				      player.handleInput(Direction.RIGHT);
+				}
+			  }
+		});
+		
 	this.model = model;
 	}
 
@@ -34,6 +68,8 @@ public class GameComponent extends JComponent {
 	g2.fill(rect);
 	
 	g2.setColor(origColor);
+	
+	player.draw(g2);
 	
 
 	// TODO: draw based on model state
